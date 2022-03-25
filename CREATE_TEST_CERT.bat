97726f024fc1fc9a8a88e5b9a9edd0f9b4f869b4
horsicq <horsicq@gmail.com>
2022-03-26 00:27:15 +0100

    Fix: 2022-03-26

---
+++ CREATE_TEST_CERT.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.17134.0\x86\makecert.exe" -r -pe -ss PrivateCertStore -n CN=Windows(TEST) -eku 1.3.6.1.5.5.7.3.3 Test.cer
+rem CertMgr /add Test.cer /s /r localMachine root
+rem certmgr -add Test.cer -s -r localMachine TRUSTEDPUBLISHER
