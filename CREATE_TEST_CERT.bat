e9cc33d714bb311ad846dd8d458180aa5c8b0bf4
horsicq <horsicq@gmail.com>
2022-04-06 08:52:45 +0200

    Fix: 2022-04-06

--- CREATE_TEST_CERT.bat
+++ CREATE_TEST_CERT.bat
@@ -1,3 +1,4 @@
+rem TODO find makecert.exe
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.17134.0\x86\makecert.exe" -r -pe -ss PrivateCertStore -n CN=Windows(TEST) -eku 1.3.6.1.5.5.7.3.3 Test.cer
 
 rem CertMgr /add Test.cer /s /r localMachine root
