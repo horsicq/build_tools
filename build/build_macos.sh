725f750bf0fecbcd3031b21fdd1666eacd4e2b09
horsicq <horsicq@gmail.com>
2024-03-28 00:04:00 +0100

    Update file(s): 2024-03-28

--- build/build_macos.sh
+++ build/build_macos.sh
@@ -1,1 +1,1 @@
-export PATH="/Application/CMake.app/Contents/bin:$PATH"cd ..mkdir tmp_buildcd tmp_buildcmake -DCMAKE_BUILD_TYPE=Release ..make
+export PATH="~/Qt/Tools/CMake/CMake.app/Contents/bin:$PATH"cd ..mkdir tmp_buildcd tmp_buildcmake -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_PREFIX_PATH="~/Qt/5.15.2/clang_64" ..makePack -G ZIP
--- build/build_msvc2019_x64.bat
+++ build/build_msvc2019_x64.bat
@@ -2,7 +2,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxili
 cd ..
 mkdir tmp_build
 cd tmp_build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="C:\Qt\5.15.2\msvc2019" -G "NMake Makefiles" ..
+cmake -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_PREFIX_PATH="C:\Qt\5.15.2\msvc2019" -G "NMake Makefiles" ..
 nmake
 cpack -G ZIP
 
