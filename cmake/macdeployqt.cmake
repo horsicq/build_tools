538e74ad98120b20c4c0cf35435ed5ae938e9aaf
horsicq <horsicq@gmail.com>
2025-06-12 06:33:03 +0200

    Update file(s): 2025-06-12

--- cmake/macdeployqt.cmake
+++ cmake/macdeployqt.cmake
@@ -1,10 +1,11 @@
 message("macdeployqt")
 
 find_program(MACDEPLOYQT NAMES macdeployqt)
-if(NOT MACDEPLOYQT)
-    message(FATAL_ERROR "macdeployqt not found. Please install Qt tools")
+if(MACDEPLOYQT)
+    add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
+        COMMAND ${MACDEPLOYQT} "$<TARGET_BUNDLE_DIR:${PROJECT_NAME}>"
+        COMMENT "Macdeployqt the executable"
+    )
+else()
+    message("macdeployqt not found. Please install Qt tools")
 endif()
-add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
-    COMMAND ${MACDEPLOYQT} "$<TARGET_BUNDLE_DIR:${PROJECT_NAME}>"
-    COMMENT "Macdeployqt the executable"
-)
