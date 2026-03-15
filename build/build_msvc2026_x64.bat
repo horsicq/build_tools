13500b6bfd4748eb8033c6e67a1ecac7604c7fe7
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2026-03-15 18:29:00 +0100

    add MSVC2026 batch script

---
+++ build/build_msvc2026_x64.bat
@@ -0,0 +1,14 @@
+IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2026\Professional\VC\Auxiliary\Build\vcvarsall.bat" (
+	call "C:\Program Files (x86)\Microsoft Visual Studio\2026\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
+) ELSE (
+	call "C:\Program Files (x86)\Microsoft Visual Studio\2026\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+)
+cd ..
+mkdir tmp_build
+cd tmp_build
+cmake -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_PREFIX_PATH="C:\Qt\6.10.1\msvc2022_64" -G "NMake Makefiles" ..
+nmake
+cpack
+
+cd ..
+xcopy /y /E tmp_build\packages\ packages\
