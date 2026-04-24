a0a2cefe17bf6164448f3b79cbe6aa128d4a1654
horsicq <horsicq@gmail.com>
2026-04-24 18:29:38 +0200

    feat: add deploy_create_missing_ts_files function

--- cmake/deploy_translation.cmake
+++ cmake/deploy_translation.cmake
@@ -39,10 +39,114 @@ function(deploy_install_directory)
     endif()
 endfunction()
 
+function(deploy_create_missing_ts_files)
+    set(options)
+    set(oneValueArgs LUPDATE_EXECUTABLE SOURCE_DIR)
+    set(multiValueArgs SOURCE_DIRS TS_FILES LUPDATE_HINTS LUPDATE_OPTIONS)
+    cmake_parse_arguments(DEPLOY_CREATE_TS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    if(DEPLOY_CREATE_TS_SOURCE_DIR)
+        list(APPEND DEPLOY_CREATE_TS_SOURCE_DIRS "${DEPLOY_CREATE_TS_SOURCE_DIR}")
+    endif()
+
+    if(NOT DEPLOY_CREATE_TS_SOURCE_DIRS)
+        set(DEPLOY_CREATE_TS_SOURCE_DIRS "${CMAKE_CURRENT_SOURCE_DIR}")
+    endif()
+
+    if(NOT DEPLOY_CREATE_TS_TS_FILES)
+        message(WARNING "deploy_create_missing_ts_files: TS_FILES is empty.")
+        return()
+    endif()
+
+    set(_deploy_missing_ts_files "")
+    foreach(_deploy_ts_file ${DEPLOY_CREATE_TS_TS_FILES})
+        if(NOT EXISTS "${_deploy_ts_file}")
+            list(APPEND _deploy_missing_ts_files "${_deploy_ts_file}")
+        endif()
+    endforeach()
+
+    if(NOT _deploy_missing_ts_files)
+        return()
+    endif()
+
+    foreach(_deploy_source_dir ${DEPLOY_CREATE_TS_SOURCE_DIRS})
+        if(NOT EXISTS "${_deploy_source_dir}")
+            message(FATAL_ERROR "deploy_create_missing_ts_files: SOURCE_DIR does not exist: ${_deploy_source_dir}")
+        endif()
+    endforeach()
+
+    set(_deploy_lupdate_executable "")
+    if(DEPLOY_CREATE_TS_LUPDATE_EXECUTABLE)
+        set(_deploy_lupdate_executable "${DEPLOY_CREATE_TS_LUPDATE_EXECUTABLE}")
+    endif()
+
+    if(NOT _deploy_lupdate_executable)
+        foreach(_deploy_lupdate_target Qt6::lupdate Qt5::lupdate)
+            if(TARGET ${_deploy_lupdate_target})
+                get_target_property(_deploy_lupdate_target_location ${_deploy_lupdate_target} IMPORTED_LOCATION)
+                if(_deploy_lupdate_target_location)
+                    set(_deploy_lupdate_executable "${_deploy_lupdate_target_location}")
+                    break()
+                endif()
+            endif()
+        endforeach()
+    endif()
+
+    if(NOT _deploy_lupdate_executable)
+        set(_deploy_lupdate_hints ${DEPLOY_CREATE_TS_LUPDATE_HINTS})
+        if(CMAKE_PREFIX_PATH)
+            foreach(_deploy_qt_prefix_path ${CMAKE_PREFIX_PATH})
+                list(APPEND _deploy_lupdate_hints "${_deploy_qt_prefix_path}/bin")
+            endforeach()
+        endif()
+
+        find_program(_deploy_lupdate_program
+            NAMES lupdate lupdate.exe
+            HINTS ${_deploy_lupdate_hints}
+        )
+
+        if(_deploy_lupdate_program)
+            set(_deploy_lupdate_executable "${_deploy_lupdate_program}")
+        endif()
+    endif()
+
+    if(NOT _deploy_lupdate_executable)
+        string(REPLACE ";" "\n  " _deploy_missing_ts_message "${_deploy_missing_ts_files}")
+        message(FATAL_ERROR
+            "lupdate was not found, and missing translation source files cannot be generated:\n"
+            "  ${_deploy_missing_ts_message}"
+        )
+    endif()
+
+    foreach(_deploy_ts_file ${_deploy_missing_ts_files})
+        get_filename_component(_deploy_ts_dir "${_deploy_ts_file}" DIRECTORY)
+        file(MAKE_DIRECTORY "${_deploy_ts_dir}")
+    endforeach()
+
+    execute_process(
+        COMMAND "${_deploy_lupdate_executable}"
+            -recursive
+            ${DEPLOY_CREATE_TS_LUPDATE_OPTIONS}
+            ${DEPLOY_CREATE_TS_SOURCE_DIRS}
+            -ts ${_deploy_missing_ts_files}
+        RESULT_VARIABLE _deploy_lupdate_result
+        OUTPUT_VARIABLE _deploy_lupdate_output
+        ERROR_VARIABLE _deploy_lupdate_error
+    )
+
+    if(NOT _deploy_lupdate_result EQUAL 0)
+        message(FATAL_ERROR
+            "Failed to create missing translation source files.\n"
+            "${_deploy_lupdate_output}\n"
+            "${_deploy_lupdate_error}"
+        )
+    endif()
+endfunction()
+
 function(deploy_add_translations)
     set(options ADD_TO_ALL)
-    set(oneValueArgs TARGET_NAME INSTALL_DESTINATION OUTPUT_DIR WINDOWS_APPDATA_SUBDIR)
-    set(multiValueArgs TS_FILES LRELEASE_HINTS)
+    set(oneValueArgs TARGET_NAME INSTALL_DESTINATION OUTPUT_DIR WINDOWS_APPDATA_SUBDIR SOURCE_DIR LUPDATE_EXECUTABLE)
+    set(multiValueArgs TS_FILES LRELEASE_HINTS SOURCE_DIRS LUPDATE_HINTS LUPDATE_OPTIONS)
     cmake_parse_arguments(DEPLOY "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     if(NOT DEPLOY_TARGET_NAME)
@@ -61,6 +165,23 @@ function(deploy_add_translations)
         message(WARNING "deploy_add_translations: TS_FILES is empty.")
         return()
     endif()
+
+    set(_deploy_create_ts_args
+        TS_FILES ${DEPLOY_TS_FILES}
+        SOURCE_DIRS ${DEPLOY_SOURCE_DIRS}
+        LUPDATE_HINTS ${DEPLOY_LUPDATE_HINTS}
+        LUPDATE_OPTIONS ${DEPLOY_LUPDATE_OPTIONS}
+    )
+
+    if(DEPLOY_SOURCE_DIR)
+        list(APPEND _deploy_create_ts_args SOURCE_DIR "${DEPLOY_SOURCE_DIR}")
+    endif()
+
+    if(DEPLOY_LUPDATE_EXECUTABLE)
+        list(APPEND _deploy_create_ts_args LUPDATE_EXECUTABLE "${DEPLOY_LUPDATE_EXECUTABLE}")
+    endif()
+
+    deploy_create_missing_ts_files(${_deploy_create_ts_args})
 
     set(_lrelease_hints ${DEPLOY_LRELEASE_HINTS})
     if(CMAKE_PREFIX_PATH)
