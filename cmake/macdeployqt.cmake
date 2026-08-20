fad4027c11055a29f6976a03cdd3c51c1b77fc4d
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2026-08-20 17:54:13 +0200

    fix macdeployqt script

--- cmake/macdeployqt.cmake
+++ cmake/macdeployqt.cmake
@@ -1,17 +1,19 @@
-message("macdeployqt")
+message("Checking macdeployqt for ${PROJECT_NAME}")
 
-find_program(MACDEPLOYQT NAMES macdeployqt)
-if(MACDEPLOYQT)
-    add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
-        COMMAND ${MACDEPLOYQT} "$<TARGET_BUNDLE_DIR:${PROJECT_NAME}>"
-        COMMENT "Macdeployqt the executable"
-    )
-else()
-    if(XVMPUNPACKER_RELEASE_BUILD)
-        message(FATAL_ERROR
-            "macdeployqt is required for an XVMPUnpacker release build")
-    else()
-        message(WARNING
-            "macdeployqt not found; the build-tree app is not deployable")
+if(TARGET ${PROJECT_NAME})
+    get_target_property(_target_dir ${PROJECT_NAME} SOURCE_DIR)
+    
+    if("${_target_dir}" STREQUAL "${CMAKE_CURRENT_SOURCE_DIR}")
+        
+        find_program(MACDEPLOYQT NAMES macdeployqt)
+        if(MACDEPLOYQT)
+            add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
+                COMMAND ${MACDEPLOYQT} "$<TARGET_BUNDLE_DIR:${PROJECT_NAME}>"
+                COMMENT "Running macdeployqt on ${PROJECT_NAME}"
+            )
+        else()
+            message(WARNING "macdeployqt not found; the app bundle will not be deployable")
+        endif()
+        
     endif()
 endif()
